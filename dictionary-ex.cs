c553b8ef8cb5a5bdc0470af9e23a3a59f658dec3
Shailen Mistry <smistry@dandb.com>
2015-10-18 15:41:14 -0700

    dictionary ex

--- dictionary-ex.cs
+++ dictionary-ex.cs
@@ -5,16 +5,9 @@ public class MyDictionary
 {
     public static void Main()
     {
-        Dictionary<string, long> phonebook = new Dictionary<string, long>();
-        phonebook.Add("Alex", 4154346543);
-        phonebook["Jessica"] = 4159484588;
-        
-        if (phonebook.ContainsKey("Alex"))
-        {
-            Console.WriteLine("Alex's number is " + phonebook["Alex"]);
-        }
-
-        phonebook.Remove("Jessica");
-        Console.WriteLine("Phone book count = " + phonebook.Count);
+        Dictionary <string, int> inventory = new Dictionary <string, int> ();
+        inventory.Add("apple", 3);
+        inventory.Add("orange", 5);
+        inventory.Add("banana", 2);
     }
 }
