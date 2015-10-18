5329df15dc2335d1a55c1bc5a6ee67e28bcb0777
Shailen Mistry <smistry@dandb.com>
2015-10-18 07:55:28 -0700

    enums

---
+++ enums.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+
+public enum CarType
+{
+    Toyota = 1,
+    Honda = 2,
+    Ford = 3,
+}
+
+public class Tutorial
+{
+    public static void Main()
+    {
+        CarType myCarType = CarType.Toyota;
+    }
+}
+
